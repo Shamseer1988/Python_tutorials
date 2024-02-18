9b9de7f71f54c230ae29fd9506e51383460bf886
Shamseer1988 <m.shamsee1988@gmail.com>
2024-02-18 22:38:29 +0300

    test

---
+++ 1_Intoduction/example_1.py
@@ -0,0 +1,21 @@
+# python tutorials basics
+print("Hello This is my first tutorials")
+name = "shamseer"
+
+age = 36
+print("My name is " + name + " My age is " + str(age))
+
+# Input Examples
+user_name = input("What is your name")
+user_age = input("May i know your age ")
+age_add = input("How many age you want add ")
+print("My name is " + user_name + " my age is " + str(user_age))
+sum_1 = (int(user_age) + int(age_add))
+print("Added age is " + str(sum_1))
+
+# sting more type
+a = "This is test for len print"
+print(len(a))
+print(a[0])
+print(a[5:10])
+# git added
---
+++ 1_Intoduction/example_2.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter 3 number"))
+number2 = int(input())
+number3 = int(input())
+average = (number1 + number2 + number3) / 3
+
+print("The average for above number is " + str(average))
+
+print(type(number1))
+
+# multi line comment
+
+""" this is multi line
+    comment...
+"""
